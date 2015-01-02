6160d1360c1335c8939ba6734a47fd2d04d555c7
atcg <evanmelstad@ucla.edu>
2015-01-02 12:42:07 -0800

    Troubleshooting

--- UnitTests/main.cpp
+++ UnitTests/main.cpp
@@ -31,5 +31,5 @@ TEST_CASE( "Convert ANGSD read counts to unsigned chars for major and minor coun
 }
 
 TEST_CASE( "Calculate PWP from the binary representations of the ANGSD readcounts", "[calcPWP]") {
-    REQUIRE( calcPWPfromBinaryFile ("angsdOut.readCounts.binary", 70000, 2) == 0);
+    REQUIRE( calcPWPfromBinaryFile ("angsdOut.readCounts.binary", 74963, 2) == 0);
 }
