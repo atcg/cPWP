b0ab027e64c1c5ba9742d5bac34589640f67ebda
Evan <evanmelstad@ucla.edu>
2015-01-03 22:24:47 -0800

    Troubleshooting

--- UnitTests/main.cpp
+++ UnitTests/main.cpp
@@ -36,7 +36,7 @@ TEST_CASE( "Generate sequence reads", "[perfectReads]") {
 
 
 TEST_CASE( " Mapping first set of reads", "[mapReads]") {
-    REQUIRE( mapReads("simulatedReferenceGenome.fasta", "normalRef_R1.fastq", "normalRef_R2.fastq", "normal.bam", "10"));
+    REQUIRE( mapReads("simulatedReferenceGenome.fasta", "normalRef_R1.fastq", "normalRef_R2.fastq", "normal.bam", "10") == 0);
 }
 
 
@@ -46,7 +46,7 @@ TEST_CASE( "Generate sequence reads 2", "[perfectReads2]") {
 
 
 TEST_CASE( " Mapping second set of reads", "[mapReads2]") {
-    REQUIRE( mapReads("simulatedReferenceGenome.fasta", "mutatedRef_R1.fastq", "mutatedRef_R2.fastq", "mutated.bam", "10"));
+    REQUIRE( mapReads("simulatedReferenceGenome.fasta", "mutatedRef_R1.fastq", "mutatedRef_R2.fastq", "mutated.bam", "10") == 0);
 }
 
 
