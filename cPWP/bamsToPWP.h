8b08df21b064c6932acbcef04848eea2603d3c24
atcg <evanmelstad@ucla.edu>
2015-01-07 15:54:12 -0800

    Troubleshooting

--- cPWP/bamsToPWP.h
+++ cPWP/bamsToPWP.h
@@ -22,6 +22,6 @@ int calcPWPfromBinaryFile (std::string binaryFile, unsigned long long int numLoc
 
 //int calcPWPforRange (unsigned long long startingLocus, unsigned long long endingLocus, int numIndividuals, std::vector<unsigned char> & mainReadCountVector, std::vector<std::vector<long double>> & threadPWP, std::vector<std::vector<unsigned long long int>> & threadWeightings);
 
-int calcPWPforRange (unsigned long long startingLocus, unsigned long long endingLocus, int numIndividuals, std::vector<unsigned char> * mainReadCountVector, std::vector<std::vector<long double>> * threadPWP, std::vector<std::vector<unsigned long long int>> * threadWeightings)
+int calcPWPforRange (unsigned long long startingLocus, unsigned long long endingLocus, int numIndividuals, std::vector<unsigned char> * mainReadCountVector, std::vector<std::vector<long double>> * threadPWP, std::vector<std::vector<unsigned long long int>> * threadWeightings);
 
 #endif /* defined(__cPWP__readsToPWP__) */
