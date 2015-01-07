1b7da1136298ce881b417ac9d90519f372b981c6
atcg <evanmelstad@ucla.edu>
2015-01-07 15:28:51 -0800

    Troubleshooting

--- cPWP/bamsToPWP.cpp
+++ cPWP/bamsToPWP.cpp
@@ -244,7 +244,7 @@ int calcPWPfromBinaryFile (std::string binaryFile, unsigned long long int numLoc
             //std::thread t = std::thread(calcPWPforRange, firstLocus, finishingLocus, std::ref(readCounts), std::ref(pwpThreads[threadRunning]), std::ref(weightingsThreads[threadRunning]));
             std::cout << "Got to the function call" << std::endl;
             //calcPWPforRange(firstLocus, finishingLocus, 272, readCounts, pwp, weightings);
-            std::thread t = std::thread(calcPWPforRange, firstLocus, finishingLocus, readCounts, pwp, weightings);
+            std::thread t = std::thread(calcPWPforRange, firstLocus, finishingLocus, std::ref(readCounts), std::ref(pwp), std::ref(weightings));
         }
         
         t.join();
