0c1c934f631b9dc55856418828afdb2d4c533238
atcg <evanmelstad@ucla.edu>
2015-01-07 15:21:28 -0800

    Troubleshooting

--- cPWP/bamsToPWP.cpp
+++ cPWP/bamsToPWP.cpp
@@ -242,6 +242,7 @@ int calcPWPfromBinaryFile (std::string binaryFile, unsigned long long int numLoc
             // Since we're passing the vectors in by reference, calPWPforRange can modify pwpThreads[thread] and weightingsThreads[thread] (but not mainReadCountVector because that is declared in the function as const
             //t[threadRunning]
             //std::thread t = std::thread(calcPWPforRange, firstLocus, finishingLocus, std::ref(readCounts), std::ref(pwpThreads[threadRunning]), std::ref(weightingsThreads[threadRunning]));
+            std::cout << "Got to the function call" << std::endl;
             calcPWPforRange(firstLocus, finishingLocus, 272, readCounts, pwp, weightings);
         }
         
