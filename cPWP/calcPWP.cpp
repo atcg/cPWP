d871bf962c059cd568492c2b2c6b409b50ed5fa3
atcg <evanmelstad@ucla.edu>
2015-01-10 15:14:03 -0800

    Minor changes

--- cPWP/calcPWP.cpp
+++ cPWP/calcPWP.cpp
@@ -37,16 +37,6 @@ int calcPWPfromBinaryFile (std::string binaryFile, unsigned long long int numLoc
         std::cout << "the total size of the file is " << size << std::endl;
         std::cout << "the number of elements in the readCounts vector is: " << readCounts.size() << std::endl; // Will give the total size bytes divided by the size of one element--so it gives the number of elements
         
-        // We now have an array of numIndividuals * 2 (major and minor allele) * 1million (loci)
-        //int totalLoci = (int)size / (numIndividuals*2); // The 1 million locus file has 999,999 sites in it (because of header line)
-        //int totalLoci = size/(272*2);
-        //std::vector< std::vector<long double> > pwp(numIndividuals, std::vector<long double>(numIndividuals,0));
-        //std::vector< std::vector<unsigned long long int> > weightings(numIndividuals, std::vector<unsigned long long int>(numIndividuals,0));
-        //long double pwp[numIndividuals][numIndividuals] = {0.0}; // This is the matrix that will hold the pwp estimates
-        //unsigned long long int weightings[numIndividuals][numIndividuals] = {0.0}; // This is the matrix that will hold the weightings--need to use a long long because the values are basically equal to the coverage squared by the end
-        
-        
-        
         /* We are going to split the loci between numThreads threads. Each thread will modify two multidimensional
          vectors of the forms std::vector< std::vector<long double> > pwp(numIndividuals, std::vector<long double>(numIndividuals,0))    and   std::vector< std::vector<unsigned long long int> > weightings(numIndividuals, std::vector<unsigned long long int>(numIndividuals,0))
          
@@ -62,7 +52,14 @@ int calcPWPfromBinaryFile (std::string binaryFile, unsigned long long int numLoc
         //unsigned long long int lociPerThread = numLoci / numThreads;
         
         //unsigned long long int lociPerThread = (readCounts.size()-1)/numThreads; // loci starts with 0, so need to subtract 1 from numLoci
-        unsigned long long int lociPerThread = (unsigned long long)(numLoci-1)/(unsigned long long)numThreads;
+        if (numLoci == 0) {
+            unsigned long long int lociPerThread = (unsigned long long)(size/(numIndividuals*2))/(unsigned long long)numThreads;
+        } else {
+            unsigned long long int lociPerThread = (unsigned long long)numLoci/(unsigned long long)numThreads;
+        }
+        
+        
+        
 
         std::cout << "Initialized lociPerThread with " << numLoci << std::endl;
         
@@ -76,8 +73,6 @@ int calcPWPfromBinaryFile (std::string binaryFile, unsigned long long int numLoc
             threadsVec.push_back(std::thread(calcPWPforRange, firstLocus, finishingLocus, numIndividuals, std::ref(readCounts), std::ref(pwpThreads[threadRunning]), std::ref(weightingsThreads[threadRunning])));
         }
         
-        std::cout << "All threads completed launching" << std::endl;
-        
         // Wait on threads to finish
         for (int i = 0; i < numThreads; ++i) {
             threadsVec[i].join();
@@ -88,11 +83,10 @@ int calcPWPfromBinaryFile (std::string binaryFile, unsigned long long int numLoc
         
         
         // Now aggregate the results of the threads and print final results
-        std::vector<std::vector<long double>> weightingsSum(272, std::vector<long double>(272,0));
-        std::vector<std::vector<long double>> pwpSum(272, std::vector<long double>(272,0));
-        std::cout << "Created the final results vectors" << std::endl;
+        std::vector<std::vector<long double>> weightingsSum(numIndividuals, std::vector<long double>(numIndividuals,0));
+        std::vector<std::vector<long double>> pwpSum(numIndividuals, std::vector<long double>(numIndividuals,0));
         
-        for (int element = 0; element < 272; element++) {
+        for (int element = 0; element < numIndividuals; element++) {
             for (int comparisonElement = 0; comparisonElement <= element; comparisonElement++) {
                 for (int threadVector = 0; threadVector < numThreads; threadVector++) {
                     //std::cout << "weightingsThreads[" << threadVector << "][" << element << "][" << comparisonElement << "] = " << weightingsThreads[threadVector][element][comparisonElement] << std::endl;
@@ -175,7 +169,6 @@ int calcPWPforRange (unsigned long long startingLocus, unsigned long long ending
                 }
             }
         }
-        //std::cout << "Made it to deleting majorAlleleFreqs" << std::endl;
         delete[] majorAlleleFreqs; // Needed to avoid memory leaks
     }
     std::cout << "Finished thread ending on locus " << endingLocus << std::endl;
