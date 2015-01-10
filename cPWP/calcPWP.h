a77074af875bddc222120a342132e877bb5253f8
atcg <evanmelstad@ucla.edu>
2015-01-10 12:59:58 -0800

    Minor changes

--- cPWP/calcPWP.h
+++ cPWP/calcPWP.h
@@ -9,6 +9,7 @@
 #ifndef ____calcPWP__
 #define ____calcPWP__
 
+#include <vector>
 
 int calcPWPforRange (unsigned long long startingLocus, unsigned long long endingLocus, int numIndividuals, std::vector<unsigned char>& mainReadCountVector, std::vector<std::vector<long double>>& threadPWP, std::vector<std::vector<unsigned long long int>>& threadWeightings);
 
