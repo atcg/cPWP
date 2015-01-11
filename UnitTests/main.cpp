af202a32de1a4c61558588bdad3e7b4ceb475bcc
atcg <evanmelstad@ucla.edu>
2015-01-10 16:22:10 -0800

    Minor changes

--- UnitTests/main.cpp
+++ UnitTests/main.cpp
@@ -73,7 +73,7 @@ TEST_CASE( "Convert ANGSD read counts to unsigned chars for major and minor coun
 }
 
 TEST_CASE( "Calculate PWP from the binary representations of the ANGSD readcounts", "[calcPWP]") {
-    REQUIRE( calcPWPfromBinaryFile ("angsdOut.readCounts.binary", 0, "testingOut.pwp", 30) == 0);
+    REQUIRE( calcPWPfromBinaryFile ("angsdOut.readCounts.binary", 0, 272, "testingOut.pwp", 30) == 0);
 }
 
 
