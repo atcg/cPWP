d6708dffa2792eb50f604309086bc359c59d1b1f
atcg <evanmelstad@ucla.edu>
2015-01-10 21:24:20 -0800

    Minor changes

--- cPWP/calcPWP.cpp
+++ cPWP/calcPWP.cpp
@@ -154,7 +154,7 @@ int calcPWPforRange (unsigned long long startingLocus, unsigned long long ending
                  majorAlleleFreqs[tortoise] = (double)mainReadCountVector[majorIndex] / (double)coverages[tortoise]; // Not necessarily an int, but could be 0 or 1
 
                  if (coverages[tortoise] > 1) {
-                     unsigned long long locusWeighting = coverages[tortoise]*(coverages[tortoise]-1);
+                     unsigned long long locusWeighting = coverages[tortoise]*(coverages[tortoise]);
                      threadWeightings[tortoise][tortoise] += (unsigned long long)locusWeighting; // This is an int--discrete number of reads
      
                      threadPWP[tortoise][tortoise] += double(locusWeighting) * (2.0 * majorAlleleFreqs[tortoise] * (double(coverages[tortoise]) - double(mainReadCountVector[majorIndex]))) / (double((coverages[tortoise])-1.0));
