865650a5d8c5cd2b464e280466e9930037723494
Evan <evanmelstad@ucla.edu>
2015-02-13 23:49:55 -0800

    Testing

--- cPWP/calcCOVARchunks.cpp
+++ cPWP/calcCOVARchunks.cpp
@@ -15,7 +15,7 @@
 
 
 
-int calcCOVARfromBinaryFile (std::string binaryFile, unsigned long long int numLoci, const int numIndividuals, std::string outFile, int lociChunkSize, unsigned long long int numThreads) {
+int calcCOVARfromBinaryFile (std::string binaryFile, unsigned long long int numLoci, const int numIndividuals, std::string outFile, int lociChunkSize, const int numThreads) {
     
     std::cout << "Number of threads: " << numThreads << std::endl;
     //std::streampos size;
