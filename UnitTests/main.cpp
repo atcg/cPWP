9295fc376fce11b7a652a6caf74ea5d95bc0e277
atcg <evanmelstad@ucla.edu>
2015-02-16 12:27:13 -0800

    Added another sample

--- UnitTests/main.cpp
+++ UnitTests/main.cpp
@@ -36,6 +36,12 @@ TEST_CASE ( "Mutate a reference genome", "[mutateRefGenome]") {
 }
 
 
+TEST_CASE ( "Mutate anoter reference genome", "[mutateRefGenome2]") {
+    REQUIRE( createMutatedGenome("simulatedReferenceGenome.fasta", "simulatedReferenceGenomeMutated2.fasta", 0.02) == 0);
+    //createMutatedGenome (std::string reference, std::string mutatedReferenceFile, float percDivergent)
+}
+
+
 TEST_CASE( "Generate sequence reads 5", "[perfectReads]") {
     REQUIRE( generatePerfectReads ("simulatedReferenceGenome.fasta", 5, 100, 300, "normalRef5") == 0);
     //generatePerfectReads (std::string reference, unsigned int stagger, unsigned int readLengths, unsigned int fragmentLengths, std::string readPrefix);
@@ -60,27 +66,50 @@ TEST_CASE( "Generate sequence reads 2", "[perfectReads2]") {
     //generatePerfectReads (std::string reference, unsigned int stagger, unsigned int readLengths, unsigned int fragmentLengths, std::string readPrefix);
 }
 
-
-/*
-TEST_CASE( " Mapping second set of reads", "[mapReads2]") {
-    REQUIRE( mapReads("simulatedReferenceGenome.fasta", "mutatedRef_R1.fastq", "mutatedRef_R2.fastq", "mutated.bam", "25") == 0);
+TEST_CASE( "Generate sequence reads 2b", "[perfectReads2b]") {
+    REQUIRE( generatePerfectReads ("simulatedReferenceGenomeMutated2.fasta", 10, 100, 300, "mutatedRef2") == 0);
+    //generatePerfectReads (std::string reference, unsigned int stagger, unsigned int readLengths, unsigned int fragmentLengths, std::string readPrefix);
 }
-*/
 
 
-TEST_CASE( "Create heterozygous R1", "[createHet]") {
+
+
+
+TEST_CASE( "Create heterozygous R1", "[createHetR1]") {
     REQUIRE( createHeterozygousGenome("normalRef10_R1.fastq", "mutatedRef_R1.fastq", "hetRef_R1.fastq") == 0);
     //createHeterozygousGenome(std::string firstFile, std::string secondFile, std::string outputGenome)
 }
 
-TEST_CASE( "Create heterozygous R2", "[createHet]") {
+TEST_CASE( "Create heterozygous R2", "[createHetR1]") {
     REQUIRE( createHeterozygousGenome("normalRef10_R2.fastq", "mutatedRef_R2.fastq", "hetRef_R2.fastq") == 0);
     //createHeterozygousGenome(std::string firstFile, std::string secondFile, std::string outputGenome)
 }
 
 
+
+TEST_CASE( "Create heterozygous R1b", "[createHetR1b]") {
+    REQUIRE( createHeterozygousGenome("normalRef10_R1.fastq", "mutatedRef2_R1.fastq", "hetRef2_R1.fastq") == 0);
+    //createHeterozygousGenome(std::string firstFile, std::string secondFile, std::string outputGenome)
+}
+
+TEST_CASE( "Create heterozygous R2b", "[createHetR2b]") {
+    REQUIRE( createHeterozygousGenome("normalRef10_R2.fastq", "mutatedRef2_R2.fastq", "hetRef2_R2.fastq") == 0);
+    //createHeterozygousGenome(std::string firstFile, std::string secondFile, std::string outputGenome)
+}
+
+
+
+
+
+
 TEST_CASE( " Mapping het reads", "[mapReads2]") {
     REQUIRE( mapReads("simulatedReferenceGenome.fasta", "hetRef_R1.fastq", "hetRef_R2.fastq", "het.bam", "5") == 0);
+    //mapReads (std::string reference, std::string R1file, std::string R2file, std::string outBam, std::string threads)
+}
+
+
+TEST_CASE( " Mapping het reads 2", "[mapReads2b]") {
+    REQUIRE( mapReads("simulatedReferenceGenome.fasta", "hetRef2_R1.fastq", "hetRef2_R2.fastq", "het2.bam", "5") == 0);
     //mapReads (std::string reference, std::string R1file, std::string R2file, std::string outBam, std::string threads)
 }
 
@@ -93,18 +122,18 @@ TEST_CASE( "Run ANGSD on simulated reads", "[runANGSD]" ) {
 
 
 TEST_CASE( "Convert ANGSD read counts to unsigned chars for major and minor counts", "[convertCountsToBinary]") {
-    REQUIRE( convertANGSDcountsToBinary("angsdOut", "angsdOut.readCounts.binary", 2, 250) == 0);
+    REQUIRE( convertANGSDcountsToBinary("angsdOut", "angsdOut.readCounts.binary", 3, 250) == 0);
     //convertANGSDcountsToBinary(std::string angsdPrefix, std::string binaryOutputFileName, int numIndividuals, int readDepthMax)
 }
 
 TEST_CASE( "Calculate PWP from the binary representations of the ANGSD readcounts", "[calcPWP]") {
-    REQUIRE( calcPWPfromBinaryFile("angsdOut.readCounts.binary", 999990, 2, "testingOut.pwp", 1000, 5) == 0);
+    REQUIRE( calcPWPfromBinaryFile("angsdOut.readCounts.binary", 999990, 3, "testingOut.pwp", 1000, 5) == 0);
     //int calcPWPfromBinaryFile (std::string binaryFile, unsigned long long int numLoci, const int numIndividuals, std::string outFile, int lociChunkSize, int numThreads=30);
 }
 
 
 TEST_CASE( "Calculate covariances from the same representations of the ANGSD readcounts", "[calcCovar]") {
-    REQUIRE(  calcCOVARfromBinaryFile("angsdOut.readCounts.binary", 999990, 2, "testingOut.covar", 1000, 5)== 0);
+    REQUIRE(  calcCOVARfromBinaryFile("angsdOut.readCounts.binary", 999990, 3, "testingOut.covar", 1000, 5)== 0);
     //calcCOVARfromBinaryFile (std::string binaryFile, long long int numLoci, const int numIndividuals, std::string outFile, int lociChunkSize, const int numThreads)
 }
 
