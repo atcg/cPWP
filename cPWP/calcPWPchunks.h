d647c3de65ac0d09377a91f0c8a48203c9e35da5
atcg <evanmelstad@ucla.edu>
2017-09-09 11:14:11 -0700

    Including sample name files to generate more informative output

--- cPWP/calcPWPchunks.h
+++ cPWP/calcPWPchunks.h
@@ -13,7 +13,7 @@
 #include <string>
 
 
-int calcPWPfromBinaryFile (std::string binaryFile, unsigned long long int numLoci, const int numIndividuals, std::string outFile, int lociChunkSize, int numThreads=30);
+int calcPWPfromBinaryFile (std::string binaryFile, unsigned long long int numLoci, const int numIndividuals, std::string outFile, int lociChunkSize, std::string sampleNamesFile, int numThreads=30);
 
 
 int calcPWPforRange (unsigned long long startingLocus, unsigned long long endingLocus, int numIndividuals, std::vector<unsigned char>& mainReadCountVector, std::vector<std::vector<long double>>& threadPWP, std::vector<std::vector<unsigned long long int>>& threadWeightings);
